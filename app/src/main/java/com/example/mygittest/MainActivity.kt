125723382ef36a58f88a465c8dc7210225ba60f1
NameParky <bbbg9191@gmail.com>
2024-02-02 17:26:20 +0900

    realreal final

--- app/src/main/java/com/example/mygittest/MainActivity.kt
+++ app/src/main/java/com/example/mygittest/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         var gitText = binding.gitText
         gitText.setOnClickListener {
-            gitText.text = "이렇게하면 충돌나겠지?."
+            gitText.setText("이렇게하면 나겠지...")
             gitText.text = "깃테스트 텍스트 또또 변경!!"
         }
     }
